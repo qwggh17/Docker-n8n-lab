c444e3082916aaa4b93b6055f069edbfcaeb0761
qwggh17 <maria.savitskaya07@gmail.com>
2026-06-26 23:14:55 +0300

    Update script.py

--- full-python-script/script.py
+++ full-python-script/script.py
@@ -1,1 +1,27 @@
+items = _items
 
+binary = items[0]["binary"]["data"]
+text = binary["data"]
+
+sentences = []
+
+for sentence in text.split("."):
+    sentence = sentence.strip()
+    if sentence:
+        sentences.append(sentence)
+
+result = "\n\n".join(sentences)
+
+return [{
+    "json": {
+        "status": "done",
+        "sentences": len(sentences)
+    },
+    "binary": {
+        "data": {
+            "data": result.encode("utf-8").decode("latin1"),
+            "fileName": "out.doc",
+            "mimeType": "text/plain"
+        }
+    }
+}]
